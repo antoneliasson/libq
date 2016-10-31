94a9d0fe7160732d7d7ee4b1a4427fe576d07b0d
Anton Eliasson <antone@axis.com>
2016-10-31 13:05:06 +0100

    tests: add equality test as demo

--- test/tests.c
+++ test/tests.c
@@ -1,8 +1,23 @@
+#include <stdbool.h>
+
 #include "minunit.h"
 
 #include "ratnum.h"
 
 mpq_t a, b, c, expected;
+
+int mpq_test_eq (mpq_t expected, mpq_t actual)
+{
+        if (!mpq_equal(expected, actual)) {
+                char *ex = mpq_get_str(NULL, 10, expected);
+                char *ac = mpq_get_str(NULL, 10, actual);
+                log_err("expected: %s, actual: %s", ex, ac);
+                free(ex);
+                free(ac);
+                return false;
+        }
+        return true;
+}
 
 char *test_add()
 {
@@ -10,7 +25,7 @@ char *test_add()
         mpq_set_si(a, 1, 5);
         mpq_set_si(b, 1, 10);
         mpq_add(c, a, b);
-        mu_assert(mpq_cmp(expected, c) == 0, "not equal");
+        mu_assert(mpq_test_eq(expected, c), "not equal");
         return NULL;
 }
 
